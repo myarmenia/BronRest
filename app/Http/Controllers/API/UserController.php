e46462bbd45f8a63a01321a52e529913c0c5a2ad
Webex <Webex@example.com>
2022-06-28 15:36:15 +0400

    phone number

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -11,12 +11,13 @@ use Illuminate\Support\Facades\Http;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Foundation\Auth\EmailVerificationRequest;
+use App\Services\MessageService;
 
 class UserController extends Controller
 {
     public function getPass(Request $request)
     {
-        
+
          $validator = Validator::make($request->all(), [
             'email' => 'required|string|email|max:255',
             'password' => 'required|string|min:6',
@@ -26,13 +27,14 @@ class UserController extends Controller
         if ($validator->fails()) {
             return response()->json($validator->errors(), 422);
         }
-        
+
         $response = Http::asForm()->post(url('/oauth/token'), [
             'grant_type' => 'password',
             'client_id' => env('PASSPORT_CLIENT_ID'),
             'client_secret' => env('PASSPORT_CLIENT_SECRET'),
             'username' => $request->email,
             'password' => $request->password,
+            'phone_number' => null,
             'scope' => '',
         ]);
         return $response->json();
@@ -52,14 +54,65 @@ class UserController extends Controller
         }
 
         $userRep = new UserRepository();
-        $userRep->create($request->all())
-            ->sendEmailVerificationNotification();
+        $userRep->create($request->all());
 
         return response()->json([
-            'message' => 'Verify Your Email'
-        ], 422);
+            'message' => 'Verify Your Phone Number'
+        ], 200);
 
 
+    }
+
+    public function registerPhone(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'phone_number' => 'required'
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 422);
+        }
+
+        $rnd = rand(1000,9999);
+        \Cache::put($request->phone_number, $rnd, $seconds = 300);
+
+        $m = new MessageService();
+        $m->sendSMS($request->phone_number,$rnd);
+        return response()->json([
+            'message' => 'Check Your Phone, after 5 minutes the code disappears',
+            'phone_number' => $request->phone_number
+        ], 200);
+
+
+    }
+
+    public function checkPhone(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'phone_number' => 'required',
+            'code' => 'required'
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 422);
+        }
+
+        $value = \Cache::get($request->phone_number);
+
+        if($value && $value == $request->code)
+        {
+           $u = Auth::user()->update(['phone_number'=> $request->phone_number]);
+
+           return response()->json([
+            'message' => 'Your Phone Number Saved Success',
+            'phone_number' => $request->phone_number
+            ], 200);
+        }
+
+        return response()->json([
+            'message' => 'Incorrect Code',
+            'phone_number' => $request->phone_number
+            ], 422);
     }
 
 
---
+++ app/Http/Controllers/SessionController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class SessionController extends Controller
+{
+    public function accessSessionData(Request $request) {
+
+     }
+     public function storeSessionData(Request $request) {
+
+     }
+     public function deleteSessionData(Request $request) {
+
+     }
+}
